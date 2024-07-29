0a7d555987b178ab00dccb274bb2fa63a2365559
Daniel08311380 <153905793+Daniel08311380@users.noreply.github.com>
2024-07-29 14:12:08 +0330

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,99 @@
+"use client";
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { Link } from "@nextui-org/link";
+import clsx from "clsx";
+import React, { useState } from "react";
+import { Card, CardHeader, CardBody, CardFooter, Divider, Image } from "@nextui-org/react";
+
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { Navbar } from "@/components/navbar";
+
+// export const metadata: Metadata = {
+//   title: {
+//     default: siteConfig.name,
+//     template: %s - ${siteConfig.name},
+//   },
+//   description: siteConfig.description,
+//   icons: {
+//     icon: "/favicon.ico",
+//   },
+// };
+
+const reverseFunction = (word: string): string => {
+  let newWord = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    newWord += word[i];
+  }
+  return newWord;
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [reverseEffect, setReverseEffect] = useState("");
+
+  const listner = () => {
+    const inputValue = (
+      document.getElementById("input-reverse") as HTMLInputElement
+    ).value;
+    const result = reverseFunction(inputValue);
+    setReverseEffect(result);
+  };
+
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className="relative flex flex-col h-screen justify-center items-center">
+            <Card className="w-96">
+              <CardHeader className="flex gap-3">
+                <div className="flex flex-col">
+                  <h2 className="text-md">Reverse Word</h2>
+                </div>
+              </CardHeader>
+              <Divider />
+              <CardBody className="flex justify-center items-center gap-y-5">
+                <Input
+                  id="input-reverse"
+                  isRequired
+                  type="text"
+                  label="Enter your word:"
+                  defaultValue=""
+                  className="max-w-xs"
+                />
+                <div className="w-52 h-9 rounded-lg bg-slate-700 flex justify-center items-center" id="reverseEffect">
+                  {reverseEffect}
+                </div>
+                <Button color="secondary" onClick={listner}>
+                  Reverse
+                </Button>
+              </CardBody>
+              <CardFooter></CardFooter>
+            </Card>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
